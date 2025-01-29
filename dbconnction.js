c54eb07685527ea10b52e15665feabf55e36d609
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2025-01-29 18:59:27 +0530

    create database table

--- dbconnction.js
+++ dbconnction.js
@@ -3,11 +3,17 @@ var db=require('mysql');
 var con=db.createConnection({
     host:"Localhost",
     user:"root",
-    password:""
+    password:"",
+    database:"mydb"
 
 });
 
 con.connect(function(err){
     if(err) throw err;
     console.log("connected");
+    var sql="create table customers(id int primary key auto_increment,name varchar(255),address varchar(255))";
+    con.query(sql,function(err, result){
+        if(err) throw err;
+        console.log("table creted");
+    })
 })
