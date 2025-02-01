138596315e275ee5b4be78b436144114a0eb361b
Iroshan99 <ampiamarakoon@std.appsc.sab.ac.lk>
2025-02-01 23:48:33 +0530

    fetch data using get method

--- expressex.js
+++ expressex.js
@@ -1,9 +1,17 @@
 var express= require('express');
 var app=express();
+var fs=require('fs');
 
 app.get('/',function(req,res){
     res.send("hello world");
 });
+
+app.get('/users',function(req,res){
+    fs.readFile('users.json','utf-8',function(err,data){
+        res.end(data.toString());
+    })
+
+})
 
 app.listen(3000,function () {
     console.log("sever is running");
